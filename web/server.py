2561355721a4d3d3ab9f3159cc771c32dfa3ecd8
LeandroKlug <leandro.softnetwork@gmail.com>
2019-08-19 21:30:49 -0300

    Adicionar função Jinja

--- web/server.py
+++ web/server.py
@@ -1,11 +1,26 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route("/")
-def hello():
-    return '<a href="proxima">Clicar aqui</a>'
+from flask import Flask, render_template
 
-@app.route("/proxima")
-def proxima():
-    return '<a href="/">Clique de volta</a>'
+app = Flask(__name__)
+
+@app.route("/index")
+def index():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/price")
+def price():
+    return render_template('price.html') 
+
+@app.route("/people")
+def people():
+    return render_template('people.html')       
+	
+	# popular com dados ficticios e depois jogar para inicio do arquivo
+	pessoas=[
+		Pessoa = ("001", "Blumenau", "São Paulo", "6", "1", "Avião")
+	]
 
 app.run(debug=True)

28231d3509ce9ce7ca7d8d907c986cd11bf2d792
leandro.klug <leandro.klug@workdb.com.br>
2019-09-16 18:48:30 -0300

    Alteração de template

--- web/model.py
+++ web/model.py
@@ -1,10 +1,29 @@
 class Viagem:
 
-	def __init__(self, nome="", local="", inicio="", volta=""):
+	nome = ""
+	local = ""
+	ida = ""
+	volta = ""
+	carro = ""
+	onibus = ""
+	aviao = ""
 
-		self.nome = nome
-		self.local = local
-		self.inicio = inicio
-		self.volta = volta
+	def __init__(self, 
+		model_nome 		= "", 
+		model_local 	= "", 
+		model_ida 		= "", 
+		model_volta 	= "", 
+		model_carro 	= "",
+		model_onibus 	= "", 
+		model_aviao 	= ""
+		):
+
+		self.nome = model_nome
+		self.local = model_local
+		self.ida = model_ida
+		self.volta = model_volta
+		self.carro = model_carro
+		self.onibus = model_onibus
+		self.aviao = model_aviao
 
 
